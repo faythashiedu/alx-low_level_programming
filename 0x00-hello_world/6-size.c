81287fcb0bd1ca295e9f3b8020c7834123c338a5
Faith Ashiedu <faithashiedu9@gmail.com>
2022-10-17 18:36:10 -0700

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 
-/**                                                                                                                                     
+/**
  * main - Entry Point
  *
- * Return: Always 0 (Success)    
- */                                                                                                       
+ * Return: Always 0 (Success)
+ */
 
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        printf("Size of a char: %lu Byte(s)\n", sizeof(char));
+int main(void)
+{
+		printf("Size of a char: %lu Byte(s)\n", sizeof(char));
 		printf("Size of a int: %lu Byte(s)\n", sizeof(int));
 		printf("Size of a long int: %lu Byte(s)\n", sizeof(long int));
 		printf("Size of a long long int: %lu Byte(s)\n", sizeof(long long int));
 		printf("Size of a float: %lu Byte(s)\n", sizeof(float));
+		printf("\n");
 		return (0);
 
-
-}			
+}
