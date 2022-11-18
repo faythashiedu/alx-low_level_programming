3bf2b476824151d60293b88476d7a63ffb4a37c2
Faith Ashiedu <faithashiedu9@gmail.com>
2022-11-18 09:33:45 -0800

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an interger
+ * @array: array to search integer
+ * @size: size of array
+ * @cmp: pointer to comparing function
+ *
+ * Return: index to the first elemeent
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i] != 0)
+				return (i);
+		}
+	}
+}
